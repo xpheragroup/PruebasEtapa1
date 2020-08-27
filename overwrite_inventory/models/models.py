6136c34a60ceae818c05498fcb9e110b50ffcd9e
Fortich <ajsuarezf@unal.edu.co>
2020-08-27 18:26:32 -0500

    Module overwrite and changes inventory funcionality to fulfill requeriments.

---
+++ overwrite_inventory/models/models.py
@@ -0,0 +1,74 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+from odoo.osv import expression
+from odoo.tools import float_compare, float_is_zero
+from odoo.addons.base.models.ir_model import MODULE_UNINSTALL_FLAG
+
+
+class Inventory(models.Model):
+    _inherit = "stock.inventory"
+
+    def _get_inventory_lines_values(self):
+        # TDE CLEANME: is sql really necessary ? I don't think so
+        locations = self.env['stock.location']
+        if self.location_ids:
+            locations = self.env['stock.location'].search([('id', 'child_of', self.location_ids.ids)])
+        else:
+            locations = self.env['stock.location'].search([('company_id', '=', self.company_id.id), ('usage', 'in', ['internal', 'transit'])])
+        domain = ' sq.location_id in %s AND pp.active'
+        args = (tuple(locations.ids),)
+
+        vals = []
+        Product = self.env['product.product']
+        # Empty recordset of products available in stock_quants
+        quant_products = self.env['product.product']
+
+        # If inventory by company
+        if self.company_id:
+            domain += ' AND sq.company_id = %s'
+            args += (self.company_id.id,)
+        if self.product_ids:
+            domain += ' AND sq.product_id in %s'
+            args += (tuple(self.product_ids.ids),)
+
+        self.env['stock.quant'].flush(['company_id', 'product_id', 'quantity', 'location_id', 'lot_id', 'package_id', 'owner_id'])
+        self.env['product.product'].flush(['active'])
+        self.env.cr.execute("""SELECT sq.product_id, sum(sq.quantity) as product_qty, sq.location_id, sq.lot_id as prod_lot_id, sq.package_id, sq.owner_id as partner_id
+            FROM stock_quant sq
+            LEFT JOIN product_product pp
+            ON pp.id = sq.product_id
+            WHERE %s
+            GROUP BY sq.product_id, sq.location_id, sq.lot_id, sq.package_id, sq.owner_id """ % domain, args)
+
+        for product_data in self.env.cr.dictfetchall():
+            product_data['company_id'] = self.company_id.id
+            product_data['inventory_id'] = self.id
+            # replace the None the dictionary by False, because falsy values are tested later on
+            for void_field in [item[0] for item in product_data.items() if item[1] is None]:
+                product_data[void_field] = False
+            product_data['theoretical_qty'] = product_data['product_qty']
+            if self.prefill_counted_quantity == 'zero':
+                product_data['product_qty'] = 0
+            if product_data['product_id']:
+                product_data['product_uom_id'] = Product.browse(product_data['product_id']).uom_id.id
+                quant_products |= Product.browse(product_data['product_id'])
+            vals.append(product_data)
+        return vals
+
+    def _action_done(self):
+        negative = next((line for line in self.mapped('line_ids') if line.product_qty < 0 and line.product_qty != line.theoretical_qty), False)
+        not_checked = next((line for line in self.mapped('line_ids') if not line.revisado), False)
+        print(not_checked)
+        if negative:
+            raise UserError(_('You cannot set a negative product quantity in an inventory line:\n\t%s - qty: %s') % (negative.product_id.name, negative.product_qty))
+        if not_checked:
+            raise UserError(_('No se ha revisado algún producto.'))
+        self.action_check()
+        self.write({'state': 'done'})
+        self.post_inventory()
+        return True
+
+class InventoryLine(models.Model):
+    _inherit = "stock.inventory.line"
+
+    revisado = fields.Boolean('Revisado', required=True)
