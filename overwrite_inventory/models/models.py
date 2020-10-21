1cc982acf466ffbc6bb5057c77c700c600005fb9
Fortich <ajsuarezf@unal.edu.co>
2020-10-21 15:00:53 -0500

    Editing picking to add parents

--- overwrite_inventory/models/models.py
+++ overwrite_inventory/models/models.py
@@ -356,6 +356,56 @@ class StockWarnInsufficientQtyScrapOver(models.TransientModel):
     def action_cancel(self):
         return self.scrap_id.to_draft()
 
+class Picking(models.Model):
+    _inherit = 'stock.picking'
+
+    parent_id = fields.Many2one(comodel_name='stock.picking')
+    children_ids = fields.One2many(comodel_name='stock.picking', inverse_name='parent_id')
+
+    @api.model
+    def create(self, vals):
+        if vals.get('origin', False):
+            parent = self.env['stock.picking'].search([['name', '=', vals['origin'].split('Retorno de ')[-1]]])
+            if parent:
+                vals['parent_id'] = parent.id
+        
+        defaults = self.default_get(['name', 'picking_type_id'])
+        picking_type = self.env['stock.picking.type'].browse(vals.get('picking_type_id', defaults.get('picking_type_id')))
+        if vals.get('name', '/') == '/' and defaults.get('name', '/') == '/' and vals.get('picking_type_id', defaults.get('picking_type_id')):
+            if picking_type.sequence_id:
+                vals['name'] = picking_type.sequence_id.next_by_id()
+
+        # As the on_change in one2many list is WIP, we will overwrite the locations on the stock moves here
+        # As it is a create the format will be a list of (0, 0, dict)
+        moves = vals.get('move_lines', []) + vals.get('move_ids_without_package', [])
+        if moves and vals.get('location_id') and vals.get('location_dest_id'):
+            for move in moves:
+                if len(move) == 3 and move[0] == 0:
+                    move[2]['location_id'] = vals['location_id']
+                    move[2]['location_dest_id'] = vals['location_dest_id']
+                    # When creating a new picking, a move can have no `company_id` (create before
+                    # picking type was defined) or a different `company_id` (the picking type was
+                    # changed for an another company picking type after the move was created).
+                    # So, we define the `company_id` in one of these cases.
+                    picking_type = self.env['stock.picking.type'].browse(vals['picking_type_id'])
+                    if 'picking_type_id' not in move[2] or move[2]['picking_type_id'] != picking_type.id:
+                        move[2]['picking_type_id'] = picking_type.id
+                        move[2]['company_id'] = picking_type.company_id.id
+        # make sure to write `schedule_date` *after* the `stock.move` creation in
+        # order to get a determinist execution of `_set_scheduled_date`
+        scheduled_date = vals.pop('scheduled_date', False)
+        res = super(Picking, self).create(vals)
+        if scheduled_date:
+            res.with_context(mail_notrack=True).write({'scheduled_date': scheduled_date})
+        res._autoconfirm_picking()
+
+        # set partner as follower
+        if vals.get('partner_id'):
+            for picking in res.filtered(lambda p: p.location_id.usage == 'supplier' or p.location_dest_id.usage == 'customer'):
+                picking.message_subscribe([vals.get('partner_id')])
+
+        return res
+
 class ProductionOver(models.Model):
     _inherit = 'mrp.production'
 
