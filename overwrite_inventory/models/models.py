102487e21f6e134d6d52c14e728e7595ee74bdb4
Fortich <ajsuarezf@unal.edu.co>
2020-11-09 12:04:16 -0500

    Stock Picking: Check different lot id if group id

--- overwrite_inventory/models/models.py
+++ overwrite_inventory/models/models.py
@@ -426,6 +426,19 @@ class Picking(models.Model):
 
         return res
 
+    def _check_different_lot_stock_moves(self):
+        if self.group_id:
+            pickings_on_group = self.env['stock.picking'].search([['group_id', '=', self.group_id.id], ['state', '=', 'done']])
+            if len(pickings_on_group) > 0:
+                move_lot_ids = []
+                for picking in pickings_on_group:
+                    for move in picking.move_line_ids_without_package:
+                        move_lot_ids.append(move.lot_id.id)
+                for move in self.move_line_ids_without_package:
+                    if move.lot_id.id not in move_lot_ids:
+                        raise UserError(_('No se puede agregar lotes no existentes en movimientos terminados anteriores. {}'.format(move.product_id.name)))
+
+
     def button_validate(self):
         self.ensure_one()
         if self.state == 'waiting':
@@ -502,12 +515,13 @@ class Picking(models.Model):
                 'context': self.env.context,
             }
 
+        self._check_different_lot_stock_moves()
+
         # Check backorder should check for other barcodes
         if self._check_backorder():
             return self.action_generate_backorder_wizard()
         self.action_done()
         return
-
 class ProductionOver(models.Model):
     _inherit = 'mrp.production'
 
