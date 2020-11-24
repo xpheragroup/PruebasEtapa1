dd3764c3245520bb78c1dae97c9df5a0e98731d1
Fortich <ajsuarezf@unal.edu.co>
2020-11-24 11:33:03 -0500

    Fixed account_general_ledger.py. Col positions were wrong. VAT is now inside partner name

--- overwrite_accounting/models/account_general_ledger.py
+++ overwrite_accounting/models/account_general_ledger.py
@@ -1,87 +1,36 @@
 # No member error in this file are for inheritance in Odoo!
 # pylint: disable=E1101
 
-from odoo import models, api, _
+from odoo import models, api, _, fields
 from odoo.tools.misc import format_date
 
 # The original definition is done in account_reports/models/account_general_ledger.py !
 
+
 class AccountGeneralLedgerReport(models.AbstractModel):
-    _inherit = "account.general.ledger"                                                 # Inherit class to make changes on account general ledger
+    # Inherit class to make changes on account general ledger
+    _inherit = "account.general.ledger"
 
-    filter_hierarchy = True                                                             # Overdefinition to remember class member
-
-    @api.model              
-    def _get_columns_name(self, options):                                               # Overdefinition to add partner VAT column (NIT in Colombia)
-        return [
-            {'name': ''},
-            {'name': _('Date'), 'class': 'date'},
-            {'name': _('Communication')},
-            {'name': _('NIT')},                                                         # Added partner VAT here!                     
-            {'name': _('Partner')},
-            {'name': _('Currency'), 'class': 'number'},
-            {'name': _('Debit'), 'class': 'number'},
-            {'name': _('Credit'), 'class': 'number'},
-            {'name': _('Balance'), 'class': 'number'}
-        ]
-
-    @api.model  
-    def _get_aml_line(self, options, account, aml, cumulated_balance):                  # Overdefinition to add partner VAT column (NIT in Colombia)
-        if aml['payment_id']:
-            caret_type = 'account.payment'
-        elif aml['move_type'] in ('in_refund', 'in_invoice', 'in_receipt'):
-            caret_type = 'account.invoice.in'
-        elif aml['move_type'] in ('out_refund', 'out_invoice', 'out_receipt'):
-            caret_type = 'account.invoice.out'
-        else:
-            caret_type = 'account.move'
-
-        if aml['ref'] and aml['name']:
-            title = '%s - %s' % (aml['name'], aml['ref'])
-        elif aml['ref']:
-            title = aml['ref']
-        elif aml['name']:
-            title = aml['name']
-        else:
-            title = ''
-
-        if aml['currency_id']:
-            currency = self.env['res.currency'].browse(aml['currency_id'])
-        else:
-            currency = False
-
-        return {
-            'id': aml['id'],
-            'caret_options': caret_type,
-            'class': 'top-vertical-align',
-            'parent_id': 'account_%d' % aml['account_id'],
-            'name': aml['move_name'],
-            'columns': [
-                {'name': format_date(self.env, aml['date']), 'class': 'date'},
-                {'name': self._format_aml_name(aml['name'], aml['ref'], aml['move_name']), 'title': title, 'class': 'whitespace_print'},
-                {'name': aml['partner_vat'], 'title': aml['partner_vat'], 'class': 'whitespace_print'}, #Added here!
-                {'name': aml['partner_name'], 'title': aml['partner_name'], 'class': 'whitespace_print'},
-                {'name': currency and aml['amount_currency'] and self.format_value(aml['amount_currency'], currency=currency, blank_if_zero=True) or '', 'class': 'number'},
-                {'name': self.format_value(aml['debit'], blank_if_zero=True), 'class': 'number'},
-                {'name': self.format_value(aml['credit'], blank_if_zero=True), 'class': 'number'},
-                {'name': self.format_value(cumulated_balance), 'class': 'number'},
-            ],
-            'level': 4,
-        }
+    # Overdefinition to remember class member
+    filter_hierarchy = True
 
     @api.model
-    def _get_query_amls(self, options, expanded_account, offset=None, limit=None):      # Overdefinition to bring parner.vat
-        unfold_all = options.get('unfold_all') or (self._context.get('print_mode') and not options['unfolded_lines'])
+    # Overdefinition to bring parner.vat
+    def _get_query_amls(self, options, expanded_account, offset=None, limit=None):
+        unfold_all = options.get('unfold_all') or (
+            self._context.get('print_mode') and not options['unfolded_lines'])
 
         if expanded_account:
             domain = [('account_id', '=', expanded_account.id)]
         elif unfold_all:
             domain = []
         elif options['unfolded_lines']:
-            domain = [('account_id', 'in', [int(line[8:]) for line in options['unfolded_lines']])]
+            domain = [('account_id', 'in', [int(line[8:])
+                                            for line in options['unfolded_lines']])]
 
         new_options = self._force_strict_range(options)
-        _, where_clause, where_params = self._query_get(new_options, domain=domain)
+        _, where_clause, where_params = self._query_get(
+            new_options, domain=domain)
         ct_query = self._get_query_currency_table(options)
         query = '''
             SELECT
@@ -101,8 +50,7 @@ class AccountGeneralLedgerReport(models.AbstractModel):
                 ROUND(account_move_line.balance * currency_table.rate, currency_table.precision) AS balance,
                 account_move_line__move_id.name         AS move_name,
                 company.currency_id                     AS company_currency_id,
-                partner.name                            AS partner_name,
-                partner.vat                             AS partner_vat,
+                partner.vat || ' ' || partner.name             AS partner_name,
                 account_move_line__move_id.type         AS move_type,
                 account.code                            AS account_code,
                 account.name                            AS account_name,
