cab06edd61b2620f20f25ccc3da83c1226da41c1
Fortich <ajsuarezf@unal.edu.co>
2020-12-18 17:16:45 -0500

    Negative inventory check

--- overwrite_inventory/models/button_confirm.py
+++ overwrite_inventory/models/button_confirm.py
@@ -1,4 +1,4 @@
-from odoo import models
+from odoo import models, fields
 
 
 class button_confirm(models.TransientModel):
@@ -8,6 +8,20 @@ class button_confirm(models.TransientModel):
         query = [['id', '=', self._context['srap']]]
         stock_scrap = self.env['stock.scrap'].search(query)
         stock_scrap.action_validate_second_confirm()
+        return {
+            'type': 'ir.actions.act_window_close'
+        }
+
+
+class ButtonConfirmGeneric(models.TransientModel):
+    _name = "overwrite_inventory.button.confirm.generic"
+
+    message = fields.Char(readonly=True)
+
+    def button_confirm(self):
+        query = [['id', '=', self._context['id']]]
+        row = self.env[self._context['model']].search(query)
+        row.button_validate_confirm()
         return {
             'type': 'ir.actions.act_window_close'
         }
