14cf807f16d0ade19561c7ab6cf4a98c7531fd83
Cristian Izaquita <cristian.izaquita@gmail.com>
2021-03-15 12:08:24 -0500

    Add: Products iteration excel report.

--- overwrite_mrp/models/material_list_group.py
+++ overwrite_mrp/models/material_list_group.py
@@ -17,18 +17,17 @@ except ImportError:
 
 
 class BomRegister(models.Model):
-
     _name = 'overwrite_mrp.bom_register'
     _description = 'A register of material list'
     # _inherit = 'report.report_xlsx.abstract'
     _rec_name = 'name_menu'
 
     boms_id = fields.Many2many(
-            string='Lista',
-            comodel_name='mrp.bom'
-        )
+        string='Lista',
+        comodel_name='mrp.bom'
+    )
 
-    name_menu = fields.Char(String='Nombre Menú',index=True)
+    name_menu = fields.Char(String='Nombre Menú', index=True)
 
     def add_product(data, bom, total):
         """Añade los datos relevantes de un producto al diccionario 'data'.
@@ -37,13 +36,13 @@ class BomRegister(models.Model):
         data: dict      -- Diccionario de datos
         bom: mrp.bom    -- Lista de materiales de la que proviene el producto
         total:int       -- Cantidad total de veces que se repetira la lista
-        
+
         """
 
         warehouse = bom.bom_id.picking_type_id[0].warehouse_id[0]
         warehouse_availible = warehouse.lot_stock_id[0].quant_ids
 
-        warehouse_availible =  list(filter(lambda q: q.product_id == bom.product_id, warehouse_availible))
+        warehouse_availible = list(filter(lambda q: q.product_id == bom.product_id, warehouse_availible))
         availible_qty = sum(q.quantity for q in warehouse_availible)
 
         reserved = sum(q.reserved_quantity for q in warehouse_availible)
@@ -53,7 +52,7 @@ class BomRegister(models.Model):
             data[bom.product_id.id]['availible_qty'] = availible_qty
             data[bom.product_id.id]['reserved'] = reserved
             data[bom.product_id.id]['warehouse'] = warehouse.name
-            
+
         else:
             data[bom.product_id.id] = {
                 'product': bom.product_id,
@@ -63,7 +62,6 @@ class BomRegister(models.Model):
                 'qty': bom.product_qty * total,
                 'uom': bom.product_uom_id
             }
-    
 
     ##TODO: Para la cantidad reservada recorrer los stock_moves de las listas (sacar asi los productos?)
     def get_all_products(self):
@@ -140,7 +138,6 @@ class BomRegister(models.Model):
 
         workbook.close()
 
-
     def action_generate_xlxs_ex2(self):
         workbook = xlsxwriter.Workbook('hello.xlsx')
         worksheet = workbook.add_worksheet()
@@ -216,11 +213,12 @@ class wizard_excel_report(models.Model):
     excel_file = fields.Binary('excel file')
     file_name = fields.Char('Excel File', size=64)
 
+
 ### This model Should not exist
 class BomGroup(models.Model):
-
     _name = 'overwrite_mrp.bom_group'
     _description = 'This model Should not exist'
+
 
 class BomRegisterXlsx(models.AbstractModel):
     _name = 'report.overwrite_mrp.menu_xlsx'
@@ -243,14 +241,15 @@ class BomRegisterXlsx(models.AbstractModel):
 
         index = 0
         for bom in lines.boms_id:
-            sheet.write(2+index,1, bom.product_id.name, format2)
-            sheet.write(2+index,2, bom.cost_center.name, format2)
-            sheet.write(2+index,3, bom.cycle, format2)
-            sheet.write(2+index,4, bom.quantity, format2)
-            sheet.write(2+index,5, bom.repetitions, format2)
-            sheet.write(2+index,6, bom.total, format2)
+            sheet.write(2 + index, 1, bom.product_id.name, format2)
+            sheet.write(2 + index, 2, bom.cost_center.name, format2)
+            sheet.write(2 + index, 3, bom.cycle, format2)
+            sheet.write(2 + index, 4, bom.quantity, format2)
+            sheet.write(2 + index, 5, bom.repetitions, format2)
+            sheet.write(2 + index, 6, bom.total, format2)
 
             index = index + 1
+
 
 class BomRegisterProductsXlsx(models.AbstractModel):
     _name = 'report.overwrite_mrp.productos_menu_xlsx'
@@ -274,6 +273,7 @@ class BomRegisterProductsXlsx(models.AbstractModel):
         sheet.write(1, 7, 'Unidades de Medida', format11)
 
         products = {}
+
         for bom in lines.boms_id:
             for child_bom in bom.bom_line_ids:
                 for inner_bom in child_bom.child_line_ids:
@@ -282,13 +282,19 @@ class BomRegisterProductsXlsx(models.AbstractModel):
                 if len(child_bom.child_line_ids) == 0:
                     BomRegister.add_product(products, child_bom, bom.total)
 
+        _logger.debug(products)
+
+        data = lines.get_all_products()
+
+        _logger.debug(products)
+
         index = 0
-        for product in products:
-            sheet.write(2+index, 1, product.product.warehouse.display_name, format2)
-            sheet.write(2+index, 2, product.product.name, format2)
-            sheet.write(2+index, 3, product.availible_qty, format2)
-            sheet.write(2+index, 4, product.qty - product.availible_qty, format2)
-            sheet.write(2+index, 5, product.qty - product.availible_qty, format2)
-            sheet.write(2+index, 6, product.qty, format2)
-            sheet.write(2+index, 7, product.uom.name, format2)
+        for product in data.products:
+            sheet.write(2 + index, 1, product.warehouse.display_name, format2)
+            sheet.write(2 + index, 2, product.product.name, format2)
+            sheet.write(2 + index, 3, product.availible_qty, format2)
+            sheet.write(2 + index, 4, product.qty - product.availible_qty, format2)
+            sheet.write(2 + index, 5, product.qty - product.availible_qty, format2)
+            sheet.write(2 + index, 6, product.qty, format2)
+            sheet.write(2 + index, 7, product.uom.name, format2)
             index = index + 1
