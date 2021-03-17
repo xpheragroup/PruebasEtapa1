f12af1451c2f23d2a458671f13766b5c0efd1d52
Cristian Izaquita <cristian.izaquita@gmail.com>
2021-03-17 12:49:10 -0500

    Add: existences_qty field for stock.move allows to compute value and show it in mrp production model.

--- overwrite_mrp/models/stock_move.py
+++ overwrite_mrp/models/stock_move.py
@@ -16,6 +16,7 @@ class Override_StockMove(models.Model):
     deviation_per = fields.Float(string='Desviación Porcentual', compute='_compute_custom_values')
     real_cost = fields.Float(string='Costo real', compute='_compute_custom_values', digits=(12,4))
     std_cost = fields.Float(string='Costo estándar', compute='_compute_custom_values', digits=(12,4))
+    existence_qty = fields.Float(string='Existencias', compute='_compute_existence_qty', digits=(12,4))
 
     @api.depends('std_quantity', 'product_qty', 'product_id.standard_price', 'product_uom_qty', 'reserved_availability', 'product_id.qty_available')
     def _compute_custom_values(self):
@@ -37,3 +38,21 @@ class Override_StockMove(models.Model):
             record.deviation_per = record.deviation / record.std_quantity if record.std_quantity > 0 else 1
             record.real_cost = record.product_uom_qty * record.product_id.standard_price
             record.std_cost = record.std_quantity * record.product_id.standard_price
+
+
+    @api.depends('product_id.qty_available', 'location_id')
+    def _compute_existence_qty(self):
+        """
+        Calcula las existencias en inventario y las muestra en el stock_move del modelo MRP
+        :return:
+        existence_qty: float
+        """
+
+        for record in self:
+            existences_temp = 0
+
+            for product_stock_quant in record.location_id.quant_ids:
+                if product_stock_quant.product_id == record.product_id:
+                    existences_temp = product_stock_quant.quantity
+
+            record.existence_qty = existences_temp
